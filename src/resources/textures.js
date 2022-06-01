77f1514cd2a2408d8ebd8469bd336cdaa3d282cd
Szymon Wilczek <kazikwilczek7@gmail.com>
2022-06-01 23:57:45 +0200

    Zapraszamy!

---
+++ src/resources/textures.js
@@ -0,0 +1,62 @@
+//billboardTextures
+let billboardTextures = {};
+billboardTextures.terpSolutionsTexture = '../src/jsm/terpSolutions.png';
+billboardTextures.bagHolderBetsTexture =
+  '../src/jsm/Bagholdersbetsbillboard.png';
+billboardTextures.homeSweetHomeTexture =
+  '../src/jsm/home-sweet-home-portrait.png';
+
+//box textures
+let boxTexture = {};
+boxTexture.Github = '../src/jsm/githubLogo.png';
+boxTexture.twitter = '../src/jsm/twitter.png';
+boxTexture.LinkedIn = '../src/jsm/linkedInLogo.png';
+boxTexture.mail = '../src/jsm/envelope.png';
+boxTexture.globe = '../src/jsm/thunder.png';
+boxTexture.reactIcon = '../src/jsm/react.png';
+boxTexture.allSkills = '../src/jsm/allSkills.png';
+boxTexture.lensFlareMain = '../src/jsm/lensflare0.png';
+boxTexture.skrillex = '../src/jsm/skrillex.png';
+boxTexture.edmText = '../src/jsm/EDM.png';
+boxTexture.writing = '../src/jsm/writing.png';
+
+//material textures
+let stoneTexture = '../src/jsm/stone.png';
+let woodTexture = '../src/jsm/woodTexture.jpg';
+
+//text
+let inputText = {};
+inputText.terpSolutionsText = '../src/jsm/terp-solutions-text.png';
+inputText.activities = '../src/jsm/activities_text.png';
+inputText.bagholderBetsText = '../src/jsm/bagholderbets-text.png';
+inputText.homeSweetHomeText = '../src/jsm/home-sweet-home-text.png';
+inputText.staticPortfolio = '../src/jsm/static-portfolio.png';
+
+//SVG
+let SVG = {};
+SVG.reactLogo = '../src/jsm/react-svg.svg';
+
+//URLs
+let URL = {};
+URL.terpsolutions =
+  'https://palmdream.com/';
+URL.ryanfloyd = 'http://radzionkow.net';
+URL.bagholderBets = 'https://github.com/Walkyst/lavalink-fork';
+URL.homeSweetHomeURL = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+URL.gitHub = 'https://github.com/szymonwilczek';
+URL.twitter = 'https://twitter.com/szymon_wilczek';
+URL.LinkedIn = 'https://www.linkedin.com/in/szymon-wilczek-b83b41161/';
+URL.email = 'https://mailto:szymonwilczek@outlook.com';
+URL.githubBagholder = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+URL.githubHomeSweetHome = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+URL.devTo =
+  'http://www.4lo.bytom.pl/';
+
+export {
+  billboardTextures,
+  boxTexture,
+  inputText,
+  URL,
+  stoneTexture,
+  woodTexture,
+};
